f55fa1fd77f28d170e63b40cea20599280866e31
Antonio GA <antoniogvx@outlook.es>
2025-12-30 21:30:44 +0100

    Subida archivos

---
+++ submit_Primes_mendel-alumno08.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --chdir=./lab-python-advan      
+#SBATCH -p hpc-bio-mendel
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+module load anaconda/2025.06
+
+ipython  primes-par-alumno08.ipynb "$1"
+
+module unload anaconda/2025.06
+
